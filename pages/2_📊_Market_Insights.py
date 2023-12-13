2c3cb2df4a16dc9f687f719b4e313bb2206c5f78
Brian Chuwa <hello@brian.co.tz>
2023-12-13 11:46:21 +0300

    testing deployment

--- pages/2_📊_Market_Insights.py
+++ pages/2_📊_Market_Insights.py
@@ -11,4 +11,4 @@ st.set_page_config(
 main_css = open("styles/main_pages.css", encoding='utf-8').read()
 st.markdown(f'<style>{main_css}</style>', unsafe_allow_html=True)
 
-st.write("# Coming Soon! ")
+st.write("# Coming Soon!! ")
